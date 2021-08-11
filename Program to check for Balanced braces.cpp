dbb4d5a000d9d133e2c4aa234d05d575e98be2e7
guruprasadas <70814406+guruprasadas@users.noreply.github.com>
2021-08-11 15:27:50 +0530

    Program to check Balanced braces using cpp

---
+++ Program to check for Balanced braces.cpp
@@ -0,0 +1,73 @@
+# include <iostream>
+#include <stdio.h>
+using namespace std;
+
+bool isvalid( string s)
+{
+  int n = s.size();
+  stack <char> st;
+  bool ans = true;
+  for(int i=0; i<n; i++)
+  {
+    if(s[i] == '(' or s[i] == '[' or s[i] == '{')
+    {
+      st.push(s[i]);
+    }
+    else if(s[i] == ')')
+    {
+      if(!st.empty() and st.top()=='(')
+      {
+        st.pop();
+      }
+      else
+      {
+        ans = false;
+        break;
+      }
+    }
+    else if(s[i] == ']')
+    {
+      if(!st.empty() and st.top()=='[')
+      {
+        st.pop();
+      }
+      else
+      {
+        ans = false;
+        break;
+      }
+    }
+    else if(s[i] == '}')
+    {
+      if(!st.empty() and st.top()=='{')
+      {
+        st.pop();
+      }
+      else
+      {
+        ans = false;
+        break;
+      }
+    }
+    
+  }
+  if(!st.empty())
+    {
+      return false;
+    }
+    return ans;
+}
+
+int main()
+{
+  string s = "{([])};
+    if(isvalid(s))
+    {
+      cout<<"Valid";
+    }
+  else
+  {
+    cout<<"Invalid";
+  }
+  return 0;
+}
